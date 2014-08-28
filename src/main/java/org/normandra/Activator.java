0d21cbdbe4afb6b92d67d631652a4e13f019f962
Alex Bowen <bowen.a@gmail.com>
2014-08-27 21:00:26 -0400

    added osgi activator

---
+++ src/main/java/org/normandra/Activator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.normandra;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * main bundle activator
+ *
+ * @author bowen
+ */
+public class Activator implements BundleActivator
+{
+    @Override
+    public void start(final BundleContext context) throws Exception
+    {
+        
+    }
+
+    @Override
+    public void stop(final BundleContext context) throws Exception
+    {
+
+    }
+}
