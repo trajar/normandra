6b1f563cbfb32147dcf2f595e141953b6ef03041
Alex Bowen <bowen.a@gmail.com>
2014-10-20 12:38:53 -0400

    added identifiable

---
+++ src/main/java/org/normandra/Identifiable.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.normandra;
+
+/**
+ * an element that has a single unique identifier
+ *
+ * @author bowen
+ */
+public interface Identifiable
+{
+    Object getId();
+}
